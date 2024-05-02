c56fda6822dc304c939e025acdcf5b6b6a701afc
LannisRaurus <lannisraurus@gmail.com>
2024-05-02 16:43:06 +0100

    New Structure 2

---
+++ GLOBAL ANALYSER/src/includes.h
@@ -0,0 +1,24 @@
+// Base C++ Includes
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+// ROOT- CERN Includes
+#include "TApplication.h"
+#include "TSystem.h"
+
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TStyle.h"
+
+#include "TH1F.h"
+#include "TH1.h"
+#include "TF1.h"
