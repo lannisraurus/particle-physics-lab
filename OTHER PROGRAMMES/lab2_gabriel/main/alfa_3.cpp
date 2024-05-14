62a08b4ecad71a0fe91c8754571463e82cdb1afa
Gabineto727 <gabrielneto444@gmail.com>
2024-05-14 10:51:00 +0100

    new data

--- OTHER PROGRAMMES/lab2_gabriel/main/alfa_3.cpp
+++ OTHER PROGRAMMES/lab2_gabriel/main/alfa_3.cpp
@@ -2,52 +2,53 @@
 
 int main(){
     
-    data_colector alfa("alfa1_data.txt");
-    vector<double> x = alfa.get_a_certain_col(1);
-    vector<double> FWHM = alfa.get_a_certain_col(2);
-    vector<double> N = alfa.get_a_certain_col(3);
+    data_colector alfa("alfa_3_data.txt");
+    vector<double> x = alfa.get_a_certain_col(2);
+    //vector<double> FWHM = alfa.get_a_certain_col(2);
+    //vector<double> N = alfa.get_a_certain_col(3);
     vector<double> ex;
-    vector<double> y;
+    vector<double> y = alfa.get_a_certain_col(1);
     vector<double> ey;
     Int_t points_numb = x.size();
     double peak_energy_MeV = 5.3044;
     double chn = 372.45;
     double echn = 11.15 / 2.355 / sqrt(1861);
+    double e_peak_p_units_MeV = 5.14235;
 
-    for (int i = 0; i < x.size(); i++){
+    /*for (int i = 0; i < x.size(); i++){
         
         ex.push_back(FWHM[i] / 2.355 / sqrt(N[i]));
-    }
+    }*/
     
     for (int i = 0; i < x.size(); i++){
         
-        y.push_back(x[i] * peak_energy_MeV / chn);
-        ey.push_back(sqrt(pow(- x[i] * peak_energy_MeV / pow(chn, 2)  * echn, 2) + pow(peak_energy_MeV / chn * ex[i], 2)));
+        y.push_back(y[i] * peak_energy_MeV / e_peak_p_units_MeV);
+
     }
 
     double* x_ptr = &x[0];
-    double* ex_ptr = &ex[0];
+    //double* ex_ptr = &ex[0];
     double* y_ptr = &y[0];
-    double* ey_ptr = &ey[0];
+    //double* ey_ptr = &ey[0];
 
     TApplication* A = new TApplication("A", 0, 0);
     TCanvas* C = new TCanvas("C", "Canvas", 16*70, 9*70);
-    TGraphErrors* g = new TGraphErrors(points_numb, x_ptr, y_ptr, ex_ptr, ey_ptr);
+    TGraph* g = new TGraph(points_numb, x_ptr, y_ptr);
     TF1* f = new TF1("f", "[0] * x + [1]");
-    TPaveText* pt = new TPaveText(225, 7, 325, 9, "user");
+    TPaveText* pt = new TPaveText(225, 5, 325, 6, "user");
 
     pt->SetTextSize(0.03);
     pt->SetFillColor(0);
     pt->SetTextAlign(12);
     pt->SetTextFont(42);
-    g->SetMarkerStyle(4);
+    g->SetMarkerStyle(8);
     g->SetMarkerColor(kBlack);
     g->GetXaxis()->SetTitle("Channels");
     g->GetXaxis()->SetLimits(200, 750);
     g->GetXaxis()->SetNdivisions(-210);
     g->GetXaxis()->SetLabelSize(0.028);
     g->GetYaxis()->SetTitle("Real energy (MeV)");
-    g->GetYaxis()->SetRangeUser(2, 10);
+    g->GetYaxis()->SetRangeUser(4, 7);
     g->GetYaxis()->SetNdivisions(-508);
     g->GetYaxis()->SetLabelSize(0.028);
     f->SetLineColor(kCyan);
