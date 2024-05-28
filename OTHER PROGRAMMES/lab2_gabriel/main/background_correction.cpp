e234451042edf9b94c6c7148894363e4f8670e80
Gabineto727 <gabrielneto444@gmail.com>
2024-05-28 09:09:23 +0100

    6th push LAB8

--- OTHER PROGRAMMES/lab2_gabriel/main/background_correction.cpp
+++ OTHER PROGRAMMES/lab2_gabriel/main/background_correction.cpp
@@ -2,17 +2,18 @@
 
 int main(){
     
-    data_colector beta("cal_gamma.txt");
-    vector<double> x = beta.get_a_certain_col(1);
-    vector<double> ex = beta.get_a_certain_col(2);
-    vector<double> y = beta.get_a_certain_col(3);
-    vector<double> ey;
+    data_colector gamma1("LAB8_CSPB0.ASC");
+    vector<double> x = gamma1.get_a_certain_col(1);
+    vector<double> y = gamma1.get_a_certain_col(2);
+    data_colector gamma2("LAB7_FUNDO.ASC");
+    vector<double> x = gamma2.get_a_certain_col(1);
+    vector<double> y = gamma2.get_a_certain_col(2);
     Int_t points_numb = x.size();
 
     for (int i = 0; i < x.size(); i++){
 
-        ey.push_back(0);
+
     }
-    
+
     return 0;
 }
