f996218cd6dd89f909d0e7b2c6b260ab579058ff
Gabineto727 <gabrielneto444@gmail.com>
2024-05-28 10:04:50 +0100

    10th push LAB8

--- OTHER PROGRAMMES/lab2_gabriel/main/background_correction.cpp
+++ OTHER PROGRAMMES/lab2_gabriel/main/background_correction.cpp
@@ -2,11 +2,13 @@
 
 int main(){
     
-    data_colector gamma1("LAB8_CSPB0.ASC");
+    string name = "LAB8_CSPB0.ASC";
+    data_colector gamma1(name);
     vector<double> x1 = gamma1.get_a_certain_col(1);
     vector<double> y1 = gamma1.get_a_certain_col(2);
     Int_t seconds1;
-    data_colector gamma2("LAB7_FUNDO.ASC");
+    string bg = "LAB7_FUNDO.ASC";
+    data_colector gamma2(bg);
     vector<double> x2 = gamma2.get_a_certain_col(1);
     vector<double> y2 = gamma2.get_a_certain_col(2);
     Int_t seconds2;
@@ -16,6 +18,35 @@ int main(){
 
         y1[i] = (y1[i] / seconds1 - y2[i] / seconds2) * seconds1;
     }
+    
+    string bg_corr_name = "bg_corr_";
+
+    bg_corr_name.append(name);
+
+    new_notepad.open(bg_corr_name);
+
+    for (int i = 0; i < data[0].size(); i++){
+        
+        for (int j = 0; j < data.size(); j++){
+            
+            if (j == data.size() - 1){
+                
+                new_notepad << data[j][i];
+            }
+            
+            else {
+
+                new_notepad << data[j][i] << " ";
+            }
+        }
+        
+        if (i < data[0].size() - 1){
+
+            new_notepad << endl;
+        }
+    }
+
+    new_notepad.close();
 
     return 0;
 }
