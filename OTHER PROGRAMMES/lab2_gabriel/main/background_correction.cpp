4a15a1958bd1cb9091939e368a1357d936a21743
Gabineto727 <gabrielneto444@gmail.com>
2024-06-03 10:24:15 +0100

    corrected files

--- OTHER PROGRAMMES/lab2_gabriel/main/background_correction.cpp
+++ OTHER PROGRAMMES/lab2_gabriel/main/background_correction.cpp
@@ -4,28 +4,35 @@ double get_seconds(string file_name);
 
 int main(){
     
-    string name = "LAB8_CSPB0.ASC"; // mudar ficheiro a retirar o fundo
+    string name = "LAB8_M2.ASC"; // mudar o nome do ficheiro a retirar o fundo
     data_colector gamma1(name);
     vector<double> x1 = gamma1.get_a_certain_col(1);
     vector<double> y1 = gamma1.get_a_certain_col(2);
     double seconds1;
     Int_t points_numb = x1.size();
-    string bg = "LAB7_FUNDO.ASC"; // mudar fundo
+    string bg = "LAB8_FUNDO2.ASC"; // mudar o nome do ficheiro de fundo
     data_colector gamma2(bg);
-    vector<double> x2 = gamma2.get_a_certain_col(1);
     vector<double> y2 = gamma2.get_a_certain_col(2);
     double seconds2;
 
     seconds1 = get_seconds(name);
     seconds2 = get_seconds(bg);
 
+    cout << "Seconds main file = " << seconds1 << " | Seconds background file = " << seconds2 << endl;
+
     for (int i = 0; i < points_numb; i++){
 
         y1[i] = (y1[i] / seconds1 - y2[i] / seconds2) * seconds1;
-    }
 
+        if (y1[i] < 0){
+
+            y1[i] = 0;
+        }
+    }
+    
+    const char* home_dir = getenv("HOME");
     ofstream bg_corr_txt;
-    string bg_corr_name = "bg_corr_";
+    string bg_corr_name = string(home_dir) + "/github_lfea1/GAMMAS CORR LAB DATA/bg_corr_";
 
     bg_corr_name.append(name);
     bg_corr_txt.open(bg_corr_name);
@@ -51,9 +58,13 @@ int main(){
 double get_seconds(string file_name) {
 
     ifstream time_finder;
+
     time_finder.open(file_name);
+
     string first_line;
+
     getline(time_finder, first_line);
+
     istringstream iss(first_line);
     string skip;
     double sec;
